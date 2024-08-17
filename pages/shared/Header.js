1ff1e24fd13c03eda13ed572cbde78a520207c47
Fenil Patel <fenil9737@gmail.com>
2024-08-17 12:15:36 +0530

    Header Toggle changes

--- pages/shared/Header.js
+++ pages/shared/Header.js
@@ -1,20 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTableColumns, faHeadset, faListCheck, faCartShopping, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTableColumns,
+  faHeadset,
+  faListCheck,
+  faCartShopping,
+  faRightFromBracket,
+  faArrowRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
 import { Navigate, Routes } from "../../navigation/NavigationLib";
 import { getAuthProps } from "../../utils/AuthenticationLibrary";
 import { APIURL, getImage } from "../../services/axios/ApiEndPoints";
 import { removeAllCookiesLogOut } from "../../utils/CookieHandler";
 
 function Header() {
+  const [userDetails, setUserDetails] = useState({
+    firstName: "",
+    lastName: "",
+    profileImagePath: "",
+  });
 
-  const [userDetails , setUserDetails] = useState({
-    firstName : '',
-    lastName : '',
-    profileImagePath : '',
-  })
+  const [openMenu, setOpenMenu] = useState(false);
 
-  useEffect(()=> {
+  useEffect(() => {
     let details = getAuthProps();
     if (details != undefined) {
       setUserDetails({
@@ -23,35 +31,76 @@ function Header() {
         profileImagePath: details.ProfileImagePath,
       });
     }
-  },[])
+  }, []);
 
   const signOut = () => {
     removeAllCookiesLogOut();
-    Navigate(Routes.signIn)
-  }
+    Navigate(Routes.signIn);
+  };
+
+  const toggleHeaderMenu = () => {
+    setOpenMenu(!openMenu);
+  };
 
   return (
-    <div className="header">
-      <div className="nav-logo">
-        <img src="./Logo.svg" alt="Dashboard"/>
-      </div>
-      <div className="nav-menu">
-        <div className="menu-links" onClick={() => Navigate(Routes.dashboard)}><FontAwesomeIcon icon={faTableColumns} /> Home</div>
-        <div className="menu-links" onClick={() => Navigate(Routes.taskList)}><FontAwesomeIcon icon={faListCheck} /> Task list</div>
-        <div className="menu-links" onClick={() => Navigate(Routes.contact)}><FontAwesomeIcon icon={faHeadset} /> Contact</div>
-        <div className="menu-links" onClick={() => Navigate(Routes.product)}><FontAwesomeIcon icon={faCartShopping} /> Shopping</div>
-      </div>
-      <div className="profile-sec">
-        <div className="log-out"  onClick={() => signOut()}><FontAwesomeIcon icon={faRightFromBracket}/> Sign out</div>
-        <div className="nav-profile" onClick={() => Navigate(Routes.profile)}>
-          <div className="profile-img">
-            {/* <img src="./Profile-pic.png" alt="Profile"/> */}
-            <img src={userDetails.profileImagePath !== "" ? APIURL + getImage + "?type=" + "profileImage" + "&&fileName=" + userDetails.profileImagePath : './Profile-pic.png'} alt="Profile"/>
+    <>
+      <div className={`${openMenu === true ? "header headeropen" : "header"}`}>
+        <div className="nav-logo">
+          <img src="./Logo.svg" alt="Dashboard" />
+        </div>
+        <div className="nav-menu">
+          <div
+            className="menu-links"
+            onClick={() => Navigate(Routes.dashboard)}
+          >
+            <FontAwesomeIcon icon={faTableColumns} /> Home
           </div>
-          <span>{userDetails.firstName + " " + userDetails.lastName}</span>
+          <div className="menu-links" onClick={() => Navigate(Routes.taskList)}>
+            <FontAwesomeIcon icon={faListCheck} /> Task list
+          </div>
+          <div className="menu-links" onClick={() => Navigate(Routes.contact)}>
+            <FontAwesomeIcon icon={faHeadset} /> Contact
+          </div>
+          <div className="menu-links" onClick={() => Navigate(Routes.product)}>
+            <FontAwesomeIcon icon={faCartShopping} /> Shopping
+          </div>
+        </div>
+        <div className="profile-sec">
+          <div className="log-out" onClick={() => signOut()}>
+            <FontAwesomeIcon icon={faRightFromBracket} /> Sign out
+          </div>
+          <div className="nav-profile" onClick={() => Navigate(Routes.profile)}>
+            <div className="profile-img">
+              {/* <img src="./Profile-pic.png" alt="Profile"/> */}
+              <img
+                src={
+                  userDetails.profileImagePath !== ""
+                    ? APIURL +
+                      getImage +
+                      "?type=" +
+                      "profileImage" +
+                      "&&fileName=" +
+                      userDetails.profileImagePath
+                    : "./Profile-pic.png"
+                }
+                alt="Profile"
+              />
+            </div>
+            <span>{userDetails.firstName + " " + userDetails.lastName}</span>
+          </div>
         </div>
       </div>
-    </div>
+      <div
+        className={`${openMenu === true ? "openmenu" : "collapsmenu"}`}
+        onClick={() => toggleHeaderMenu()}
+      >
+        {openMenu === true ? (
+          <FontAwesomeIcon icon={faArrowRightFromBracket} />
+        ) : (
+          <FontAwesomeIcon icon={faArrowRightFromBracket} />
+        )}
+      </div>
+    </>
   );
 }
 
