802a168f4614c818f75ec3d0afa4359f17f09117
Viren602 <viren602@users.noreply.github.com>
2024-08-18 11:18:25 +0530

    HEader responsive

--- pages/shared/Header.js
+++ pages/shared/Header.js
@@ -6,7 +6,8 @@ import {
   faListCheck,
   faCartShopping,
   faRightFromBracket,
-  faArrowRightFromBracket,
+  faBars,
+  faXmark,
 } from "@fortawesome/free-solid-svg-icons";
 import { Navigate, Routes } from "../../navigation/NavigationLib";
 import { getAuthProps } from "../../utils/AuthenticationLibrary";
@@ -44,7 +45,7 @@ function Header() {
 
   return (
     <>
-      <div className={`${openMenu === true ? "header headeropen" : "header"}`}>
+      <div className={`header`}>
         <div className="nav-logo">
           <img src="./Logo.svg" alt="Dashboard" />
         </div>
@@ -91,14 +92,74 @@ function Header() {
         </div>
       </div>
       <div
-        className={`${openMenu === true ? "openmenu" : "collapsmenu"}`}
+        className="mobilemenu"
         onClick={() => toggleHeaderMenu()}
       >
-        {openMenu === true ? (
-          <FontAwesomeIcon icon={faArrowRightFromBracket} />
-        ) : (
-          <FontAwesomeIcon icon={faArrowRightFromBracket} />
-        )}
+        <div className="mobile-nav">
+          <div className="mobile-logo">
+            <img src="./Logo.svg" alt="Logo" />
+          </div>
+          {openMenu === true ? (
+            <FontAwesomeIcon icon={faXmark} />
+          ) : (
+            <FontAwesomeIcon icon={faBars} />
+          )}
+        </div>
+        <div className={`${openMenu === true ? 'menu-slide' : 'displaynone'}`}>
+          <div className="mobilenav-menu">
+            <div
+              className="mobilemenu-links"
+              onClick={() => Navigate(Routes.dashboard)}
+            >
+              <FontAwesomeIcon icon={faTableColumns} /> Home
+            </div>
+            <div
+              className="mobilemenu-links"
+              onClick={() => Navigate(Routes.taskList)}
+            >
+              <FontAwesomeIcon icon={faListCheck} /> Task list
+            </div>
+            <div
+              className="mobilemenu-links"
+              onClick={() => Navigate(Routes.contact)}
+            >
+              <FontAwesomeIcon icon={faHeadset} /> Contact
+            </div>
+            <div
+              className="mobilemenu-links"
+              onClick={() => Navigate(Routes.product)}
+            >
+              <FontAwesomeIcon icon={faCartShopping} /> Shopping
+            </div>
+          </div>
+          <div className="profile-sec">
+            <div className="log-out" onClick={() => signOut()}>
+              <FontAwesomeIcon icon={faRightFromBracket} /> Sign out
+            </div>
+            <div
+              className="nav-profile"
+              onClick={() => Navigate(Routes.profile)}
+            >
+              <div className="profile-img">
+                {/* <img src="./Profile-pic.png" alt="Profile"/> */}
+                <img
+                  src={
+                    userDetails.profileImagePath !== ""
+                      ? APIURL +
+                        getImage +
+                        "?type=" +
+                        "profileImage" +
+                        "&&fileName=" +
+                        userDetails.profileImagePath
+                      : "./Profile-pic.png"
+                  }
+                  alt="Profile"
+                />
+              </div>
+              <span>{userDetails.firstName + " " + userDetails.lastName}</span>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
